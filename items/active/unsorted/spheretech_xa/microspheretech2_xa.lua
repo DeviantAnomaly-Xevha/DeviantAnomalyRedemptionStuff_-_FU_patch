5569eebc4a9aa3ac800dbb98f7735cb6ac48f8a0
DeviantAnomaly <97322272+DeviantAnomaly-Xevha@users.noreply.github.com>
2022-07-28 21:06:49 -0400

    first upload

---
+++ items/active/unsorted/spheretech_xa/microspheretech2_xa.lua
@@ -0,0 +1,11 @@
+function init()
+  if player then
+    local tech = {"microspherebomb","microspherespider"}
+    for i = 1, #tech do
+      local tecc = tech[i].."_xa"
+      player.makeTechAvailable(tecc)
+      player.enableTech(tecc)
+      item.consume(1)
+    end
+  end
+end
